8e4a91cea56480cdf40e8546834a0b52200baa27
Anna-Sergeevna <4042808@gmail.com>
2021-11-05 21:29:52 +0200

    Добавляет Container и Section

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Container from 'components/Container';
+import Section from 'components/Section';
+import './App.css';
+
+export default function App() {
+  return (
+    <Container title="Виджет отзывов">
+      <Section title="Feedback options"></Section>
+      <Section title="Statistics"></Section>
+    </Container>
+  );
+}
